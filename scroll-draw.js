7aab1d120d2c989c2c340bb5809f1ced9263be87
John Johnson <johnrjj@gmail.com>
2016-12-30 18:45:05 -0500

    Add files via upload

--- scroll-draw.js
+++ scroll-draw.js
@@ -1,35 +1,35 @@
-const letters = Array.from(document.querySelectorAll('.letter--outline'));
-const lettersContainer = document.querySelector('.letters');
-const longestPath = 200; //Math.ceil(letters.reduce((x, y) => x > y.getTotalLength() ? x : y.getTotalLength(), 0));
-const getScrollPercent = () => ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
-const toggleOutline = (e) => {
+var letters = Array.from(document.querySelectorAll('.letter--outline'));
+var lettersContainer = document.querySelector('.letters');
+var longestPath = 200; //Math.ceil(letters.reduce((x, y) => x > y.getTotalLength() ? x : y.getTotalLength(), 0));
+var getScrollPercent = function () { return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100); };
+var toggleOutline = function (e) {
     e.classList.remove('letter--fill');
     e.classList.add('letter--outline');
 };
-const toggleFill = (e) => {
+var toggleFill = function (e) {
     e.classList.add('letter--fill');
     e.classList.remove('letter--outline');
 };
-const scaleUpLetters = (e) => {
+var scaleUpLetters = function (e) {
     e.classList.add('letters--scaled');
 };
-const scaleDownLetters = (e) => {
+var scaleDownLetters = function (e) {
     e.classList.remove('letters--scaled');
 };
-document.addEventListener('scroll', () => {
-    const scrollPercent = ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
-    if (scrollPercent > 99) {
+document.addEventListener('scroll', function () {
+    var scrollPercent = ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
+    if (scrollPercent > 97) {
         letters.forEach(toggleFill);
         scaleDownLetters(lettersContainer);
         return;
     }
-    letters.forEach(l => (l.classList.contains('letter--fill')) ? toggleOutline(l) : null);
+    letters.forEach(function (l) { return (l.classList.contains('letter--fill')) ? toggleOutline(l) : null; });
     lettersContainer.classList.contains('letters--scaled') ? null : scaleUpLetters(lettersContainer);
-    const offset = scrollPercent * (longestPath / 100);
-    letters.forEach(l => l.style.strokeDashoffset = (longestPath - offset).toString());
+    var offset = scrollPercent * (longestPath / 100);
+    letters.forEach(function (l) { return l.style.strokeDashoffset = (longestPath - offset).toString(); });
 });
-const init = () => {
-    letters.forEach(l => l.style.strokeDashoffset = longestPath.toString());
-    letters.forEach(l => l.style.strokeDasharray = longestPath.toString());
+var init = function () {
+    letters.forEach(function (l) { return l.style.strokeDashoffset = longestPath.toString(); });
+    letters.forEach(function (l) { return l.style.strokeDasharray = longestPath.toString(); });
 };
 init();
